67680e8fe7935ce0d04b696262cd114d51d74fa6
Sin <sin.email1100@gmail.com>
2021-07-12 21:01:46 +0800

    Routing works as expected

--- main.py
+++ main.py
@@ -3,8 +3,12 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def home():
+def index():
     return render_template("homepage.html")
+
+@app.route('/predict_by_batch')
+def table():
+    return render_template("table.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
